a042f837c179ab664df34266b65206eecab978fa
Patrick Hein <bagbag98@googlemail.com>
2019-01-31 21:27:41 +0100

    clusterized

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const Http = require('http');
+const Cluster = require('cluster');
+const OperatingSystem = require('os');
 
-const server = new Http.Server();
+const coreCount = OperatingSystem.cpus().length;
 
 const verteilers = [
   'https://verteiler1.mediathekview.de/',
@@ -10,19 +12,25 @@ const verteilers = [
   'https://verteiler.mediathekviewweb.de/'
 ];
 
+if (Cluster.isMaster) {
+  for (let i = 0; i < coreCount; i++) {
+    Cluster.fork();
+  }
+} else {
+  Http.createServer((request, response) => {
+    const verteiler = getRandomVerteiler();
+  
+    response.writeHead(301, {
+      'Location': verteiler
+    });
+  
+    response.end();
+  }).listen(8999);
+
+  console.log(`Worker ${process.pid} started`);
+}
+
 function getRandomVerteiler() {
   const index = Math.floor(Math.random() * verteilers.length);
   return verteilers[index];
 }
-
-server.on('request', (request, response) => {
-  const verteiler = getRandomVerteiler();
-
-  response.writeHead(301, {
-    'Location': verteiler
-  });
-
-  response.end();
-});
-
-server.listen(8999);
